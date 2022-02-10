1d299e53a16585e0dfc720a75973fbca43d7182b
joannetruong <truong.j@gatech.edu>
2022-02-09 20:18:38 -0500

    add single bash file for data collection

--- collect_data.sh
+++ collect_data.sh
@@ -3,4 +3,4 @@
 roscore & \
 python ros_nodes/spot_camera_node.py & \
 python ros_nodes/robot_state_node.py & \
-rosbag record -o bags/tmp.bag /spot_joint_states /spot_odom_vel /spot_pose /spot_vision_vel /spot_fl_fisheye/compressed /spot_fr_fisheye/compressed /spot_fl_depth /spot_fr_depth fasdfasd
+rosbag record -o bags/tmp.bag /spot_joint_states /spot_odom_vel /spot_pose /spot_vision_vel /spot_fl_fisheye/compressed /spot_fr_fisheye/compressed /spot_fl_depth /spot_fr_depth
