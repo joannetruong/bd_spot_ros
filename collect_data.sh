21a7413ab699cb5e262e6cf25b90980b7ad94164
joannetruong <truong.j@gatech.edu>
2022-02-14 17:20:53 -0500

    rename default bag name

--- collect_data.sh
+++ collect_data.sh
@@ -3,4 +3,4 @@
 roscore & \
 python ros_nodes/spot_camera_node.py & \
 python ros_nodes/robot_state_node.py & \
-rosbag record -o bags/tmp.bag /spot_joint_states /spot_odom_vel /spot_pose /spot_vision_vel /spot_fl_fisheye/compressed /spot_fr_fisheye/compressed /spot_l_fisheye/compressed /spot_r_fisheye/compressed /spot_b_fisheye/compressed /spot_fl_depth /spot_fr_depth /spot_l_depth /spot_r_depth /spot_b_depth
+rosbag record -o bags/spot_cam_state.bag /spot_joint_states /spot_odom_vel /spot_pose /spot_vision_vel /spot_fl_fisheye/compressed /spot_fr_fisheye/compressed /spot_l_fisheye/compressed /spot_r_fisheye/compressed /spot_b_fisheye/compressed /spot_fl_depth /spot_fr_depth /spot_l_depth /spot_r_depth /spot_b_depth
