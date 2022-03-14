49bf8b22133bb134461443d183cdcdc35eb5a6fc
joannetruong <truong.j@gatech.edu>
2022-03-14 16:14:14 -0400

    make separate scripts for position vs. velocity command test

--- record_loc_pos_test.sh
+++ record_loc_pos_test.sh
@@ -3,4 +3,4 @@
 roscore & \
 python ros_nodes/spot_state_node.py & \
 rosbag record -o bags/spot_state.bag /spot_odom_vel /spot_pose & \
-python spot_wrapper/localization_test.py & \
+python spot_wrapper/localization_test.py
